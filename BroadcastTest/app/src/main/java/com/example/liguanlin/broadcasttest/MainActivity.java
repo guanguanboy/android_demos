bd98c1135071f7a14b12cc120bc75445b372c05b
liguanlin <liguanlin@orbbec.com>
2018-10-30 19:04:46 +0800

    system broadcast demo

--- BroadcastTest/app/src/main/java/com/example/liguanlin/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/com/example/liguanlin/broadcasttest/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.liguanlin.broadcasttest;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+    private NetworkChangeReceiver networkChangeReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver {
+
+        public void onReceive(Context context, Intent intent)
+        {
+            //Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();
+
+            ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if(null != networkInfo && networkInfo.isAvailable())
+            {
+                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
