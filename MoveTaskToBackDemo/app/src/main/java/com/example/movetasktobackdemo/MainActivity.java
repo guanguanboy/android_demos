5c3786d0b9674249018be4a9d0bd90c6bd2c3864
guanguanboy <517445163@qq.com>
2019-01-17 23:49:28 +0800

    add movetasktobackdemo

---
+++ MoveTaskToBackDemo/app/src/main/java/com/example/movetasktobackdemo/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.movetasktobackdemo;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveTaskToBack(true);
+                Log.d("===", "called moveTaskToBack() method");
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        Log.e("====", "onPause()");
+        super.onPause();
+    }
+
+    /**
+     * abc
+     */
+    @Override
+    protected void onStop() {
+        Log.e("====", "onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.e("====", "onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.e("====", "onRestart()");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.e("====", "onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.e("====", "onResume()");
+        super.onResume();
+    }
+}
