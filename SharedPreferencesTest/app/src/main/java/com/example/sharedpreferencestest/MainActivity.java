ab061ccf9115effccb4c076841b3bf1cbe73d4eb
liguanlin <liguanlin@orbbec.com>
2019-02-05 17:47:09 +0800

    finish SharedPreferencesTest

--- SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
+++ SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.sharedpreferencestest;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button saveData = (Button)findViewById(R.id.save_data);
+
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                editor.putString("name", "Tom");
+                editor.putInt("age", 28);
+                editor.putBoolean("married", false);
+                editor.apply();
+            }
+        });
+
+        Button restoreData = (Button) findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+                Log.d("MainActivity", "name is " + name);
+                Log.d("MainActivity", "age is " + age);
+                Log.d("MainActivity", "married is " + married);
+            }
+        });
+    }
+
+//    @Override
+//    public void onBackPressed() {
+//        super.onBackPressed();
+//    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("MainActivity", "onDestory() called");
+        super.onDestroy();
     }
 }
