f03eab8f3bc5b887f1807d343599cd8326a32a7b
liguanlin <liguanlin@orbbec.com>
2019-02-08 11:56:51 +0800

    add code to test whether we call opencv libary successfully

--- OpenCvDemo/app/src/main/java/com/example/opencvdemo/MainActivity.java
+++ OpenCvDemo/app/src/main/java/com/example/opencvdemo/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.opencvdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this){
+
+        @Override
+        public void onManagerConnected(int status) {
+            super.onManagerConnected(status);
+
+            switch(status)
+            {
+                case BaseLoaderCallback.SUCCESS:
+                    Log.d("345", "Load successed！");
+                    break;
+
+                    default:
+                        Log.d("345", "Load failed!");
+                        break;
+            }
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!OpenCVLoader.initDebug())
+        {
+            Log.d("345", "OKOK");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0,this, mLoaderCallback);
+        }
+        else
+        {
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
     }
 }
