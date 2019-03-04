ab82e5d65da8bbf900f8e6ab0dfdbb0fcac56913
liguanlin <liguanlin@orbbec.com>
2019-03-04 21:11:27 +0800

    add insert function in DatabaseTest

--- DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.databasetest;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,12 +18,37 @@ public class MainActivity extends AppCompatActivity {
 
         dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
 
-        Button createDatabase = (Button) findViewById(R.id.create_database);
+        Button createDatabase = findViewById(R.id.create_database);
         createDatabase.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
                 dbHelper.getWritableDatabase();
+            }
+        });
+
+        Button addData = findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+                ContentValues values = new ContentValues();
+
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+
+                db.insert("book", null, values);
+                values.clear();
+
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+
+                db.insert("book", null, values);
             }
         });
     }
