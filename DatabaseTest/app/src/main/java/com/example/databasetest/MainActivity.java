4c2a6ef5807883267af92cbad3dfac2828e39c1c
liguanlin <liguanlin@orbbec.com>
2019-03-12 20:35:00 +0800

    add update delete and query functions to DatabaseTest demo

--- DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.databasetest;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -49,6 +51,57 @@ public class MainActivity extends AppCompatActivity {
                 values.put("price", 19.95);
 
                 db.insert("book", null, values);
+            }
+        });
+
+        Button updateData = findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price", 10.99);
+                db.update("book", values, "name = ?", new String[] { "The Da Vinci Code"});
+            }
+        });
+
+        Button deleteData = findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.delete("book", "pages > ?", new String[]{ "500"});
+            }
+        });
+
+        Button queryButton = findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+                Cursor cursor = db.query("book", null, null, null, null, null, null);
+                if ( cursor.moveToFirst()){
+                    do {
+
+                        //遍历Cursor对象，取出数据并打印
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+
+                        Log.d("MainActivity", "book name is " + name);
+                        Log.d("MainActivity", "book author is " + author);
+                        Log.d("MainActivity", "book pages is " + pages);
+                        Log.d("MainActivity", "book price is " + price);
+
+                    }while (cursor.moveToNext());
+                }
+
+                cursor.close();
             }
         });
     }
